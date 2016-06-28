c31bdeaf19048b969d6349b0ac6dcdb63680e468
vive <vive@vive.example.com>
2016-06-28 16:16:24 +0530

    Changed zip file version

--- src/test/java/org/wso2/carbon/connector/integration/test/outlookmail/OutLookMailConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/outlookmail/OutLookMailConnectorIntegrationTest.java
@@ -39,7 +39,7 @@ public class OutLookMailConnectorIntegrationTest extends ConnectorIntegrationTes
 	 */
 	@BeforeClass(alwaysRun = true)
 	public void setEnvironment() throws Exception {
-		init("outlookmail-connector-1.0.1");
+		init("outlookmail-connector-1.0.2-SNAPSHOT");
 
 		esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
 		esbRequestHeadersMap.put("Content-Type", "application/json");
